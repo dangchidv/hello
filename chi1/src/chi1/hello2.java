40f6e1eb5e216e1fcb6d93c2dd3daa207eefbf77
chi <dangchidv96@gmail.com>
2019-04-12 09:44:55 +0700

    demo git eclipse

--- chi1/src/chi1/hello2.java
+++ chi1/src/chi1/hello2.java
@@ -2,4 +2,10 @@ package chi1;
 
 public class hello2 {
 
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
 }
